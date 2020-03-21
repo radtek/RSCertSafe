b3c12265e61230bf407f52f971252f84738ae1af
Kevin <anyboo@qq.com>
2020-03-21 20:21:24 +0800

    Update File Version

--- Resource.h
+++ Resource.h
@@ -37,6 +37,25 @@
 #define IDD_DIALOG3                     207
 #define IDB_BITMAP1                     209
 
+#define STRINGIZE2(s)		#s
+#define STRINGIZE(s)        STRINGIZE2(s)
+
+#define MAIN				2 
+#define Major				0 
+#define Minor				1 
+#define FileIndex			38
+
+#define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
+#define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
+
+#define WMAIN				2 
+#define WMajor				0 
+#define WMinor				6 
+#define WFileIndex			79
+
+#define VTSPRODUCTVERSION			WMAIN,WMajor,WMinor,WFileIndex
+#define VTSPRODUCTVERSION_STR		STRINGIZE(WMAIN) "." STRINGIZE(WMajor) "." STRINGIZE(WMinor) "." STRINGIZE(WFileIndex)
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
