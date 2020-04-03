b5b711d4892974738f38ac4932dd0f7772771476
Kevin <anyboo@qq.com>
2020-04-03 11:21:26 +0800

    Fixed Version Number

--- Resource.h
+++ Resource.h
@@ -8,7 +8,7 @@
 #define MAIN				2 
 #define Major				0 
 #define Minor				1 
-#define FileIndex			38
+#define FileIndex			39
 
 #define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
 #define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
@@ -36,25 +36,6 @@
 #define IDD_DIALOG2                     205
 #define IDD_DIALOG3                     207
 #define IDB_BITMAP1                     209
-
-#define STRINGIZE2(s)		#s
-#define STRINGIZE(s)        STRINGIZE2(s)
-
-#define MAIN				2 
-#define Major				0 
-#define Minor				1 
-#define FileIndex			38
-
-#define VTSFILEVERSION			MAIN,Major,Minor,FileIndex
-#define VTSFILEVERSION_STR		STRINGIZE(MAIN) "." STRINGIZE(Major) "." STRINGIZE(Minor) "." STRINGIZE(FileIndex)
-
-#define WMAIN				2 
-#define WMajor				0 
-#define WMinor				6 
-#define WFileIndex			79
-
-#define VTSPRODUCTVERSION			WMAIN,WMajor,WMinor,WFileIndex
-#define VTSPRODUCTVERSION_STR		STRINGIZE(WMAIN) "." STRINGIZE(WMajor) "." STRINGIZE(WMinor) "." STRINGIZE(WFileIndex)
 
 // Next default values for new objects
 // 
